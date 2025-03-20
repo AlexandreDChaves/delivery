6909315bed69a1de28d99bc9bf99eccbaf30d107
AlexandreDChaves <xandechaves@gmail.com>
2025-03-20 14:31:23 -0300

    add rotas ao backend

--- server.js
+++ server.js
@@ -25,10 +25,41 @@ db.connect((err) => {
     console.log("Conectado com sucesso ao banco de dados")
 })
 
-app.get('/', (req, res) => {
-    res.send("Servidor ok!")
+app.get('/pedidos', (req, res) => {
+    const sql = 'SELECT * FROM pedidos'
+    db.query(sql, (err, results) => {
+        if(err) throw err
+        res.json(results)
+    })
+    
+})
+
+app.post('/pedidos', (req,res) => {
+    const {cliente, sabor_pizza, tamanho} = req.body
+    const sql = 'INSERT INTO pedidos (cliente, sabor_pizza, tamanho) VALUES (?, ?, ?)'
+    db.query(sql, [cliente, sabor_pizza, tamanho], (err, result) => {
+        if(err) throw err
+        res.status(201).json({message: 'Pedido criado com sucesso', id: result.insertId})
+    })
+})
+
+app.put('/pedidos/:id', (req, res) => {
+    const {status_pedido} = req.body
+    const sql = 'UPDATE pedidos SET status_pedido = ? WHERE id = ?'
+    db.query(sql, [status_pedido, req.params.id], (err, result) => {
+        if (err) throw err
+        res.json({message: 'Status do pedido atualizado com sucesso!'})
+    })
+})
+
+app.delete('/pedidos/:id', (req, res) => {
+    const sql = 'DELETE FROM pedidos WHERE id = ?'
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err
+        res.json({message: 'Pedido removido com sucesso'})
+    })
 })
 
 app.listen(port, () => {
-    
+
 })
